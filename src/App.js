eb74191ecd0deefcac12bb26174d82e131b0d077
usamamubarikali <usamamubarikali@gmail.com>
2020-06-07 16:41:09 +0500

    added tailwind and initializing app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red-500">
+      <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5">
+        
+        <img src="https://images.prismic.io/wonderbly/aa50d4db-5bed-4910-8d10-2fe9549fb6db_ILYTM_rangepage_i_love_you+%282%29.png?auto=compress,format&amp;rect=0,0,410,410&amp;w=410&amp;h=410);background-size:contain"
+         className="db w-100 br2 br--top" 
+         alt="photograph of a kitten looking menacing."/>
+        
+        <div className="pa2 ph3-ns pb3-ns">
+          <div className="dtc">
+            <h1 className="f5 f4-ns mv0">I Love You This Much</h1>
+          </div>
+          <p className="f6 lh-copy measure mt2 mid-gray">
+            If it fits, i sits burrow under covers. Destroy couch leave hair everywhere,
+            and touch water with paw then recoil in horror.
+          </p>
+          <div className="dt w-100 mt1">
+            <div className="dtc">
+                <h1 className="f5 f4-ns mv0">Age</h1>
+            </div>
+            <div className="dtc tr">
+              <h2 className="f5 mv0">$1,000</h2>
+            </div>
+          </div>
+        </div>
+      </article>
     </div>
   );
 }
